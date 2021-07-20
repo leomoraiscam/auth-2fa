38910698b3204be89a150547abd9764edd7633c3
lmorais <leozonnn@gmail.com>
2021-07-19 23:05:29 -0300

    chore: definition the architeture to api

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+
+import 'express-async-errors';
+import AppError from '../../errors/AppError';
+import '../typeorm';
+
+const app = express();
+app.use(express.json());
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(`Error: ${error}`);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+);
+
+export default app;
