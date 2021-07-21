5d76f1a02df678c4c0861e4a04908aea528bce94
lmorais <leozonnn@gmail.com>
2021-07-21 01:25:49 -0300

    chore: use global routes in application

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,9 +5,11 @@ import 'express-async-errors';
 import AppError from '../../errors/AppError';
 import '../typeorm';
 import '../../container';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
