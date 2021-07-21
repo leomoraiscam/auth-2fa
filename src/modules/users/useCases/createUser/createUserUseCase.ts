d3237df18a272f3aa016785a9697cf367edf13a6
lmorais <leozonnn@gmail.com>
2021-07-21 00:16:44 -0300

    feat: implementation rules to crate an user in use case

---
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -0,0 +1,35 @@
+import { hash } from 'bcryptjs';
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '../../../../shared/errors/AppError';
+import ICreateUserDTO from '../../dtos/ICreateUserDTO';
+import User from '../../infra/typeorm/entities/User';
+import IUserRepository from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UserRepository')
+    private usersRepository: IUserRepository
+  ) {}
+
+  async execute({ name, email, password }: ICreateUserDTO): Promise<User> {
+    const savedUser = await this.usersRepository.findByEmail(email);
+
+    if (savedUser) {
+      throw new AppError('user already exists', 409);
+    }
+
+    const hashPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserUseCase;
