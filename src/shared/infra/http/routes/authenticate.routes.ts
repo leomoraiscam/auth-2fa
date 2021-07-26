46cf568639845bb38f91936c978b43bbb92f6325
lmorais <leozonnn@gmail.com>
2021-07-25 23:00:42 -0300

    feat: add new route to send request to recover password

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import AuthenticatedUserController from '../../../../modules/auth/useCases/authenticateUser/authenticateController';
+import SendForgotPasswordEmailController from '../../../../modules/auth/useCases/sendForgotPasswordEmail/sendForgotPasswordEmailController';
 
-const authenticateRoutes = Router();
+const passwordRoutes = Router();
 const authenticatedUserController = new AuthenticatedUserController();
+const sendForgotPasswordEmailController =
+  new SendForgotPasswordEmailController();
 
-authenticateRoutes.post('/sessions', authenticatedUserController.handle);
+passwordRoutes.post('/sessions', authenticatedUserController.handle);
+passwordRoutes.post('/forgot', sendForgotPasswordEmailController.handle);
 
-export default authenticateRoutes;
+export default passwordRoutes;
