39fbc1651dc2de5da73d2d59db7ce43e490b9a36
lmorais <leozonnn@gmail.com>
2021-07-25 22:54:51 -0300

    feat: implementation users token repositories at in memory, core and abstract

---
+++ src/modules/auth/repositories/in-memory/UsersTokenRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import UserToken from '../../infra/typeorm/entities/UserToken';
+import IUserTokenRepository from '../IUserTokenRepository';
+
+class UserTokensRepositoryInMemory implements IUserTokenRepository {
+  private usersTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuidv4(),
+      token: uuidv4(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+
+  async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.usersTokens.find(
+      (findToken) => findToken.token === token
+    );
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepositoryInMemory;
