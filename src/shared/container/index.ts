7228dc87bd2ef9d05679b072252b8020d2d3d384
lmorais <leozonnn@gmail.com>
2021-07-25 22:58:07 -0300

    feat: add all depedencies injection in global files in shared folder

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,17 @@
 import { container } from 'tsyringe';
+import './providers';
 
+import UserTokensRepository from '../../modules/auth/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokenRepository from '../../modules/auth/repositories/IUserTokenRepository';
 import UsersRepository from '../../modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '../../modules/users/repositories/IUsersRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<IUserTokenRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
 );
