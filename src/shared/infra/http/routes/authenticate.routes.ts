fac35cb15e490fe830f0e37290663c04f1036635
lmorais <leozonnn@gmail.com>
2021-07-27 23:54:43 -0300

    feat: add new route to validate 2fa token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -2,13 +2,20 @@ import { Router } from 'express';
 
 import AuthenticatedUserController from '../../../../modules/auth/useCases/authenticateUser/authenticateController';
 import SendForgotPasswordEmailController from '../../../../modules/auth/useCases/sendForgotPasswordEmail/sendForgotPasswordEmailController';
+import ValidateTwoFactorAuthenticateTokenController from '../../../../modules/auth/useCases/validateTwoFactorAuthenticateToken/validateTwoFactorAuthenticateTokenController';
 
 const passwordRoutes = Router();
 const authenticatedUserController = new AuthenticatedUserController();
 const sendForgotPasswordEmailController =
   new SendForgotPasswordEmailController();
+const validateTwoFactorAuthenticateTokenController =
+  new ValidateTwoFactorAuthenticateTokenController();
 
 passwordRoutes.post('/sessions', authenticatedUserController.handle);
 passwordRoutes.post('/forgot', sendForgotPasswordEmailController.handle);
+passwordRoutes.post(
+  '/2fa',
+  validateTwoFactorAuthenticateTokenController.handle
+);
 
 export default passwordRoutes;
