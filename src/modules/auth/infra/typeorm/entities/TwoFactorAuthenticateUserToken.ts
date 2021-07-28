758df23bfe279798d74603fe307273111c9cb725
lmorais <leozonnn@gmail.com>
2021-07-27 23:47:32 -0300

    feat: implementation and definition 2fa tokens entity

---
+++ src/modules/auth/infra/typeorm/entities/TwoFactorAuthenticateUserToken.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Generated,
+} from 'typeorm';
+
+@Entity('two_factor_authenticate_user_tokens')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  confirmation_register: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
