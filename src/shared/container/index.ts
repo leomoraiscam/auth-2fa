49869e0ed8d3b48116563abf98d43a52cb1358af
lmorais <leozonnn@gmail.com>
2021-07-27 23:54:19 -0300

    feat: new depedency to use cases

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe';
 import './providers';
 
+import TwoFactorAuthenticateUsersTokenRepository from '../../modules/auth/infra/typeorm/repositories/TwoFactorAuthenticateUsersTokenRepository';
 import UserTokensRepository from '../../modules/auth/infra/typeorm/repositories/UserTokensRepository';
+import ITwoFactorAuthenticateUserTokenRepository from '../../modules/auth/repositories/ITwoFactorAuthenticateUserTokenRepository';
 import IUserTokenRepository from '../../modules/auth/repositories/IUserTokenRepository';
 import UsersRepository from '../../modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '../../modules/users/repositories/IUsersRepository';
@@ -14,4 +16,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokenRepository>(
   'UserTokensRepository',
   UserTokensRepository
+);
+
+container.registerSingleton<ITwoFactorAuthenticateUserTokenRepository>(
+  'TwoFactorAuthenticateUsersTokenRepository',
+  TwoFactorAuthenticateUsersTokenRepository
 );
