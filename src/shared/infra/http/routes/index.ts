33a7eb26495191386acc1ee95b1a9a011a470e32
lmorais <leozonnn@gmail.com>
2021-08-06 21:36:10 -0300

    chore: add prefix in auth routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,6 @@ import userRoutes from './user.routes';
 const routes = Router();
 
 routes.use('/users', userRoutes);
-routes.use(authenticateRoutes);
+routes.use('/auth', authenticateRoutes);
 
 export default routes;
